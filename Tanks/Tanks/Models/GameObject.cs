847d835464ef4be8f6dc19b507e0a3cb784056df
Andrey Karpov <karpowandrey13@mail.ru>
2018-04-18 19:55:35 +0300

    Some changes

--- Tanks/Tanks/Models/GameObject.cs
+++ Tanks/Tanks/Models/GameObject.cs
@@ -9,16 +9,18 @@ namespace Tanks.Models
 {
     abstract class GameObject
     {
+        private int speed;
+
         public Bitmap Sprite { get; }
         public int Width => Sprite.Width;
         public int Height => Sprite.Height;
         public double Speed { get; set; }
         public Direction Direction { get; set; }
-        public Point Position { get; set; }
+        public PointF Position { get; set; }
 
         private DateTime lastUpdate { get; set; }
 
-        public GameObject(string sprite, int speed = 0, Point position = default, Direction direction = Direction.None)
+        public GameObject(string sprite, int speed = 0, PointF position = default, Direction direction = Direction.None)
         {
             Sprite = new Bitmap(sprite);
             Speed = speed;
@@ -30,8 +32,37 @@ namespace Tanks.Models
             g.DrawImage(Sprite, Position);
         }
 
+        public bool Collides(GameObject obj)
+        {
+            bool collide = false;
+
+
+            return collide;
+        }
+
         public void Update()
         {
+            TimeSpan dt = DateTime.Now - lastUpdate;
+            PointF p = Position;
+            switch (Direction)
+            {
+                case Direction.Up:
+                    p.Y -= speed * dt.Seconds;
+                    break;
+                case Direction.Down:
+                    p.Y += speed * dt.Seconds;
+                    break;
+                case Direction.Left:
+                    p.X -= speed * dt.Seconds;
+                    break;
+                case Direction.Right:
+                    p.X += speed * dt.Seconds;
+                    break;
+                default:
+                    break;
+            }
+
+            Position = p;
             lastUpdate = DateTime.Now;
         }
     }
