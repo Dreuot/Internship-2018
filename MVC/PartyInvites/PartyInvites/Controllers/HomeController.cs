df8d1cbbbeb5f5de09ff522bf019f407cf30ddea
Andrey Karpov <karpowandrey13@mail.ru>
2018-05-05 13:34:59 +0300

    second Chapter

---
+++ MVC/PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using PartyInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour >= 12 ? "Добрый день" : "Доброе утро";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponce)
+        {
+            if (ModelState.IsValid)
+                return View("Thanks", guestResponce);
+            else
+                return View();
+        }
+    }
+}
---
+++ MVC/PartyInvites/PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Пожалуйста, введите Ваше имя.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите email адрес")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Пожалуйста, введите валидный email адрес")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите Ваш номер телефона")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, укажите, придете Вы или нет")]
+        public bool? WillAttend { get; set; }
+    }
+}
